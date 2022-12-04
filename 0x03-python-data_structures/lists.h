7968ea52cab669f6301b52a8790664fff410c807
Jakmike <asomughakenenna@gmail.com>
2022-12-04 15:43:37 -0800

    Data structure

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+        int n;
+        struct listint_s *next;
+} listint_t;
+
 def print_list_integer(my_list=[]);
 def replace_in_list(my_list, idx, element);
 
