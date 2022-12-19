69f8c7cea65c4425fd4995be22f25b8a5d3def60
Jakmike <asomughakenenna@gmail.com>
2022-12-19 07:01:47 -0800

    Editted safe print

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     i = value
-    if value is True:
-        print("{:d}".format(i))
+    try:
+        print("{}".format(i))
         return True
-    else:
+    except value is True:
         return False
